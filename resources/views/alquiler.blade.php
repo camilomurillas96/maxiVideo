0bb2a58580f155b32b49b8edee64c591c7d58e3a
camilomurillas96 <camilogonzalez_96@hotmail.com>
2021-10-13 00:09:42 -0500

    se incluye formulario en alquiler

--- resources/views/alquiler.blade.php
+++ resources/views/alquiler.blade.php
@@ -18,6 +18,43 @@
                     <input type="text" name="numeroId" class="form-control" placeholder="Numero de identificación" required="required">
                   </div>
                 </div>
+                <div class="col-sm-6">
+                  <div class="form-group">
+                    <input type="text" name="name" class="form-control" placeholder="Nombre Completo">
+                  </div>
+                </div>    
+                <div class="col-sm-6">
+                  <div class="form-group">
+                    <input type="text" name="telefono" class="form-control" placeholder="Numero teléfonico">
+                  </div>
+                </div>
+                <div class="col-sm-6">
+                  <div class="form-group">
+                    <input type="text" name="direccion" class="form-control" placeholder="Direccion de residencia">
+                  </div>
+                </div>
+              </div>
+              <div class="form-group">
+                <input type="email" name="email" class="form-control" placeholder="Correo electronico">
+              </div>
+                <hr>
+                <br>
+                <div class="row  wow fadeInUp" data-wow-duration="1000ms" data-wow-delay="300ms">
+                <div class="col-sm-6">
+                <input class="form-select" aria-label="select example" list="milist">
+                  <datalist id="milist">
+                  <option value="">Seleeccione una Pelicula</option>
+                  <option value="1">One</option>
+                  <option value="2">Two</option>
+                  <option value="3">Three</option>
+                </datalist>
+                </select>
+                <div class="invalid-feedback">Por favor, seleccione una pelicula</div>
+                </div>
+                </div>
+              <div class="form-group">
+                <textarea name="message" id="message" class="form-control" rows="4" placeholder="Observación" ></textarea>
+              </div>  
               </div>                 
               <div class="form-group">
                 <button type="submit" class="btn-submit">Send Now</button>
